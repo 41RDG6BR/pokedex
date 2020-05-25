7e0fd4793b8963abade760e7ee0b4d3519e50bc4
Rodrigo Nogueira <rdg6design@gmail.com>
2020-05-25 11:18:22 -0300

    Import Components

--- App.js
+++ App.js
@@ -17,62 +17,59 @@ import {
 } from 'react-native';
 import PokeCard from './components/PokeCard';
 import NavBar from './components/Navbar'
+import SearchBar from './components/Search'
 import { Card, ListItem, Button, Icon, Input } from 'react-native-elements'
 
 import { getPokemon, getAllPokemon } from './services/pokemon';
 
+let searched
 function App() {
   const [pokemonData, setPokemonData] = useState([])
-  const [nextUrl, setNextUrl] = useState('');
-  const [prevUrl, setPrevUrl] = useState('');
+  const [searchData, setSearchData] = useState(null)
   const [loading, setLoading] = useState(true);
-  const initialURL = 'https://pokeapi.co/api/v2/pokemon'
+  const initialURL = 'https://pokeapi.co/api/v2/pokemon/?limit=50&offset=0'
 
   useEffect(() => {
     async function fetchData() {
       let response = await getAllPokemon(initialURL)
-      setNextUrl(response.next);
-      setPrevUrl(response.previous);
       await loadPokemon(response.results);
       setLoading(false);
     }
     fetchData();
   }, [])
 
-  const next = async () => {
-    setLoading(true);
-    let data = await getAllPokemon(nextUrl);
-    await loadPokemon(data.results);
-    setNextUrl(data.next);
-    setPrevUrl(data.previous);
-    setLoading(false);
-  }
-
-  const prev = async () => {
-    if (!prevUrl) return;
-    setLoading(true);
-    let data = await getAllPokemon(prevUrl);
-    await loadPokemon(data.results);
-    setNextUrl(data.next);
-    setPrevUrl(data.previous);
-    setLoading(false);
-  }
-
+  let allPokemon = []
   const loadPokemon = async (data) => {
-    let _pokemonData = await Promise.all(data.map(async pokemon => {
+      let _pokemonData = await Promise.all(data.map(async pokemon => {
       let pokemonRecord = await getPokemon(pokemon)
       return pokemonRecord
     }))
     setPokemonData(_pokemonData);
   }
 
+  const mySearch = (text) => {
+    searched = text.toLowerCase()
+      if(searched == ''){
+      setSearchData(pokemonData)
+    } else {
+      let poke = pokemonData.filter(obj=>{
+          return obj.name.toLowerCase().startsWith(searched)
+      })
+    setSearchData(poke)
+    }
+  }
+
+  // const loadingText = (<Text>Carregando...</Text>)
+  const realData = searchData || pokemonData
   return (
         <ScrollView>
             <View>
-              <NavBar />
-                {pokemonData.map((pokemon, i) => {
+            <NavBar />
+              <SearchBar onchange={mySearch} value={searched} />
+                {realData.map((pokemon, i) => {
                   return <PokeCard key={i} pokemon={pokemon} />
                 })}
+              {/* {loadingText} */}
             </View>
         </ScrollView>
   );
